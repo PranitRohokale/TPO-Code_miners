d005187e152d726b8c32c35d8cd13c9b7b975657
PranitRohokale <pranitrohokale@gmail.com>
2023-01-12 11:32:15 +0530

    initial Template!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <>Header</>;
+};
+
+export default Header;
