661212fe47bf19e4869f770555cfc7519c8551c4
PranitRohokale <pranitrohokale@gmail.com>
2023-01-13 15:23:29 +0530

    mutation for recruter

---
+++ src/Graphql/Mutations/recruter.js
@@ -0,0 +1,29 @@
+const CREATE_NEW_RECRUTERS_MUTATION = gql`
+  mutation createNewRecruters($objects: [Recruiters_insert_input!] = []) {
+    insert_Recruiters(objects: $objects) {
+      affected_rows
+      returning {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+// {
+//     "objects": [
+//       {
+//         "id": "53ec5de4-e19f-4a02-988c-d10805680eab",
+//         "name": "pranit",
+//         "email": "pranitrohokale@gmail.com",
+//         "companyName": "GroundUp",
+//         "mobileNo": "+918446122060"
+//       }
+//     ]
+//   }
+
+
+export {
+    CREATE_NEW_RECRUTERS_MUTATION
+}
