fedc9a46ff005ba67babf7e4697035733565d59a
sahilkedare <sahilkedare4444@gmail.comm>
2023-01-13 19:37:10 +0530

    requirement form done

--- src/Screens/HR/CreateJob/CreateJob.jsx
+++ src/Screens/HR/CreateJob/CreateJob.jsx
@@ -5,6 +5,39 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from "../../../Utils/supabase.config";
 import Sidebar from "../../../Components/hrSidebar/Sidebar";
 
+
+const branches = [
+  {
+    name: "Electrical",
+  },
+  {
+    name: "Civil",
+  },
+  {
+    name: "Mechanical",
+  },
+  {
+    name: "EXTC",
+  },
+  {
+    name: "Electronics",
+  },
+  {
+    name: "Textile",
+  },
+  {
+    name: "Computer",
+  },
+  {
+    name: "IT",
+  },
+  {
+    name: "Mechanical",
+  },
+
+];
+
+
 const customStyles = {
   content: {
     top: "50%",
@@ -20,12 +53,21 @@ const customStyles = {
 };
 
 const CreateJob = () => {
+
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [salary, setSalary] = useState(0);
   const [noOfRounds, setNoOfRounds] = useState(0);
   const [endDate, setEndDate] = useState("");
   const [location, setLocation] = useState("");
+  const [ssc, setSsc] = useState(0);
+  const [hsc, setHsc] = useState(0);
+  const [cpi, setCpi] = useState(0);
+  const [allowedGender, setAllowedGender] = useState("");
+  const [allowedProgramme, setAlloweProgramme] = useState("");
+  const [addReq, setAddReq] = useState("");
+  const [timeDeadline, setTimeDeadline] = useState("11:59");
+  const [dateDeadline, setDateDeadline] = useState("");
 
   const navigate = useNavigate();
 
@@ -41,28 +83,76 @@ const CreateJob = () => {
     });
   }, []);
 
+  const [checkedState, setCheckedState] = useState(
+    new Array(branches.length).fill(false)
+  );
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!(title && desc && salary && noOfRounds && endDate && location))
-      return alert("All fields required");
+    // if (!(title && desc && salary && noOfRounds && location && dateDeadline))
+    //   return alert("All fields required");
+    console.log(title, desc, salary, noOfRounds, endDate, location, ssc, hsc, cpi, allowedGender, allowedProgramme, addReq, timeDeadline, dateDeadline);
 
-    let locationArr = location.split(",");
+    let branchArr = [];
+    checkedState.forEach((item, index) => {
+      if (item == true) {
+        branchArr.push(branches[index].name)
+      }
+    })
+    let genderArr = [];
+    let programmeArr = [];
+    if (allowedGender === "female") {
+      genderArr.push("female")
+    }
+    else if (allowedGender === "male") {
+      genderArr.push("male")
 
-    console.log(title, desc, salary, noOfRounds, endDate, locationArr);
+    }
+    else {
+      genderArr.push("female")
+      genderArr.push("male")
+    }
 
-    setTitle("");
-    setDesc("");
-    setSalary(0);
-    setNoOfRounds(0);
-    setEndDate("");
-    setLocation("");
+    if (allowedProgramme === "btech") {
+      programmeArr.push("btech")
+    }
+    else if (allowedProgramme === "mtech") {
+      programmeArr.push("mtech")
+    }
+    else {
+      programmeArr.push("btech")
+      programmeArr.push("mtech")
+
+    }
+    if (!isNaN(cpi)) {
+      setCpi(parseFloat(cpi))
+    }
+    else{
+      setCpi(0)
+    }
+    console.log(branchArr, genderArr, programmeArr, "branches")
+
+
+    // setTitle("");
+    // setDesc("");
+    // setSalary(0);
+    // setNoOfRounds(0);
+    // setEndDate("");
+    // setLocation("");
   };
 
   return (
-    <div style={{display: "flex"}}>
-      <Sidebar/>
-      <div style={{width: "100%"}}
-        // style={customStyles}
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <div style={{ width: "100%" }}
+      // style={customStyles}
       >
         <div className="flex flex-col justify-center py-2 sm:px-6 lg:px-8">
           <div className="text-center text-2xl font-bold">Create a Job</div>
@@ -105,42 +195,45 @@ const CreateJob = () => {
                     />
                   </div>
                 </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Salary
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="password"
-                      type="number"
-                      placeholder="Salary"
-                      value={salary}
-                      onChange={(e) => setSalary(Math.max(0, e.target.value))}
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div className="w-full mr-5">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Salary
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="password"
+                        type="number"
+                        placeholder="Salary"
+                        value={salary}
+                        onChange={(e) => setSalary(Math.max(0, e.target.value))}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      No of Rounds
+                    </label>
+                    <div className="mt-1 w-full">
+                      <input
+                        id="name"
+                        type="number"
+                        placeholder="No of Rounds"
+                        value={noOfRounds}
+                        onChange={(e) => setNoOfRounds(Math.max(0, e.target.value))}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
                   </div>
                 </div>
-                <div className="w-full">
-                  <label
-                    htmlFor="name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    No of Rounds
-                  </label>
-                  <div className="mt-1 w-full">
-                    <input
-                      id="name"
-                      type="number"
-                      placeholder="No of Rounds"
-                      value={noOfRounds}
-                      onChange={(e) => setNoOfRounds(Math.max(0, e.target.value))}
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
+
 
                 <div className="w-full">
                   <label
@@ -161,6 +254,198 @@ const CreateJob = () => {
                     />
                   </div>
                 </div>
+
+                <h2>Job Requirements</h2>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div className="w-full pr-5">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      10th
+                    </label>
+                    <div className="mt-1 w-full">
+                      <input
+                        id="10th"
+                        type="number"
+                        placeholder="10th"
+                        value={ssc}
+                        onChange={(e) => setSsc(Math.max(0, e.target.value))}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full pr-5">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      12th
+                    </label>
+                    <div className="mt-1 w-full">
+                      <input
+                        id="12th"
+                        type="number"
+                        placeholder="12th"
+                        value={hsc}
+                        onChange={(e) => setHsc(Math.max(0, e.target.value))}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      CPI
+                    </label>
+                    <div className="mt-1 w-full">
+                      <input
+                        id="cpi"
+                        type="text"
+                        placeholder="CPI"
+                        value={cpi}
+                        onChange={(e) => setCpi(e.target.value)}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div style={{ width: "50%", paddingRight: "20px" }}>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Allowed Programme
+                    </label>
+                    <div className="mt-1">
+                      <select
+                        value={allowedProgramme}
+                        onChange={(e) => {
+                          setAlloweProgramme(e.target.value.trim());
+                        }}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      >
+                        <option value="btech">B.Tech</option>
+                        <option value="mtech">M.Tech</option>
+                        <option value="all">All</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div style={{ width: "50%" }}>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Allowed Gender
+                    </label>
+                    <div className="mt-1">
+                      <select
+                        value={allowedGender}
+                        onChange={(e) => {
+                          setAllowedGender(e.target.value.trim());
+                        }}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      >
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="all">All</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div style={{ width: "45%" }}>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Last Date
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="dob"
+                        type="date"
+                        placeholder="DOB"
+                        value={dateDeadline}
+                        onChange={(e) => setDateDeadline(e.target.value.trim())}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div style={{ width: "45%" }}>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Time
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="dob"
+                        type="time"
+                        placeholder="DOB"
+                        value={timeDeadline}
+                        onChange={(e) => setTimeDeadline(e.target.value.trim())}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Additional Requirements
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      placeholder="Additional Requirements"
+                      value={addReq}
+                      onChange={(e) => setAddReq(e.target.value)}
+                      id="email"
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Allowed Branches
+                  </label>
+                  <ul className="">
+                    {branches.map(({ name, price }, index) => {
+                      return (
+                        <li key={index}>
+                          <div className="toppings-list-item">
+                            <div className="left-section">
+                              <input
+                                type="checkbox"
+                                id={`custom-checkbox-${index}`}
+                                name={name}
+                                value={name}
+                                checked={checkedState[index]}
+                                onChange={() => handleOnChange(index)}
+                              />
+                              <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                            </div>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+
                 <div>
                   <button
                     type="submit"
