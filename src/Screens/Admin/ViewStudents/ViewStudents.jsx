b94d724d5fb83761335decd9d7c67c04c2a24d37
soumilkamat <soumilkamat5@gmail.com>
2023-01-13 19:42:21 +0530

    student is cr checkbox

--- src/Screens/Admin/ViewStudents/ViewStudents.jsx
+++ src/Screens/Admin/ViewStudents/ViewStudents.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Snackbar,
@@ -22,7 +22,7 @@ import {
 } from "@mui/material";
 
 import { Link } from "react-router-dom";
-import StudentsTable from "../../../Components/AdminViewStudents/StudentsTable";
+// import StudentsTable from "../../../Components/AdminViewStudents/StudentsTable";
 import { GET_ALL_STUDENTS } from "../../../Graphql/Queries/AdminViewStudents/adminViewStudents";
 import { useQuery } from "@apollo/client";
 
@@ -119,28 +119,48 @@ import { useQuery } from "@apollo/client";
 
 const ViewStudents = () => {
   const { loading, error, data } = useQuery(GET_ALL_STUDENTS);
+  const [crList, setCrList] = useState({});
+  const [updatedCrs, setUpdatedCrs] = useState({});
+  useEffect(() => {
+    if (data) {
+      var temp_cr_list = {};
+      for (var student of data.Students) {
+        temp_cr_list[student.id] = student.isCr;
+      }
+      setCrList(crList);
+    }
+    console.log(crList);
+  }, []);
 
   // if(loading)
   //     return
 
-  console.log(data && data.Students);
+  //   console.log(data && data.Students);
 
   return (
     <Container maxWidth="lg" sx={{ my: 5 }}>
-    {data ? (<TableContainer component={Paper}>
-        <Table sx={{ minWidth: 350, maxWidth: 350 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Student Name</TableCell>
-              <TableCell align="right">Branch</TableCell>
-              <TableCell align="right">ID</TableCell>
-              <TableCell align="right">Grad Year</TableCell>
-              <TableCell align="right">Gender</TableCell>
-              <TableCell align="right">Is CR</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.Students.map((row, index) => (
+      {data ? (
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ minWidth: 350, maxWidth: 350 }}
+            aria-label="simple table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>Student Name</TableCell>
+                <TableCell align="right">Is CR</TableCell>
+                <TableCell align="right">Is Placed</TableCell>
+                <TableCell align="right">Programme</TableCell>
+                <TableCell align="right">Branch</TableCell>
+                <TableCell align="right">ID</TableCell>
+                <TableCell align="right">Grad Year</TableCell>
+                <TableCell align="right">Gender</TableCell>
+                <TableCell align="right">Age</TableCell>
+                <TableCell align="right">CPI</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.Students.map((row, index) => (
                 <TableRow
                   key={index}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -148,19 +168,48 @@ const ViewStudents = () => {
                   <TableCell component="th" scope="row">
                     {row.firstName + " " + row.lastName}
                   </TableCell>
-                  <TableCell align="right">{row.branch}</TableCell>
-                  <TableCell align="right">{row.clgId}</TableCell>
-                  <TableCell align="right">{row.gradYear}</TableCell>
-                  <TableCell align="right">{row.gender}</TableCell>
                   <TableCell align="right">
-                    <Checkbox checked={row.isCr} color="success" />
+                    <Checkbox
+                      checked={crList[row.id]}
+                      onChange={(e) => {
+                        var crList_ = crList;
+                        crList_[row.id] = !crList_[row.id];
+                        setCrList(crList_);
+                        var updatedCrs_ = updatedCrs;
+                        updatedCrs_[row.id] = crList[row.id];
+                        setUpdatedCrs(updatedCrs_);
+                        console.log(updatedCrs);
+                      }}
+                      color="success"
+                    />
                   </TableCell>
+                  {/* isDreamPlaced: null,
+isNormalPlaced: null,
+isSuperPlaced: null, */}
+                  <TableCell>
+                    {row.isNormalPlaced
+                      ? row.isDreamPlaced
+                        ? row.isSuperPlaced
+                          ? "Yes, Super dream"
+                          : "Yes, Dream"
+                        : "Yes, Normal"
+                      : "No"}
+                  </TableCell>
+                  <TableCell>{row.programme?row.programme:"N/A"}</TableCell>
+                  <TableCell>{row.branch?row.branch:"N/A"}</TableCell>
+                  <TableCell align="right">{row.clgId?row.clgId:"N/A"}</TableCell>
+                  <TableCell align="right">{row.gradYear?row.gradYear:"N/A"}</TableCell>
+                  <TableCell>{row.gender?row.gender:"N/A"}</TableCell>
+                  <TableCell align="right">{row.age?row.age:"N/A"}</TableCell>
+                  <TableCell align="right">{row.CPI?row.CPI:"N/A"}</TableCell>
                 </TableRow>
-              ))}   
-          </TableBody>
-        </Table>
-      </TableContainer>) : "Loading..."}
-
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        "Loading..."
+      )}
     </Container>
   );
 };
